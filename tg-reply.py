9c0e3f19bf901d96ee8565bcb663d38c6e4a30b8
gribanovdan@gmail.com <V_eronika367>
2022-05-24 19:57:37 +0300

    Add files

---
+++ tg-reply.py
@@ -0,0 +1,35 @@
+from telethon import TelegramClient, sync, events
+
+api_id = ''
+api_hash = ''
+message = 'Добрый день!\nДля связи с технической поддержкой 1it.io напишите сообщение: @123_bot'
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+myself = client.get_me()
+
+if client.is_user_authorized():
+    print('Auth OK: ' + myself.username + ', ' + myself.phone)
+
+
+@client.on(events.NewMessage)
+async def normal_handler(event):
+    sender = await client.get_entity(event.from_id)
+    if sender.username != myself.username:
+        if 'bot' not in sender.username:
+            print("==== New message ====")
+            print(str(event.message.date))
+            print("username: " + sender.username)
+            if sender.phone is not None:
+                print("phone: " + str(sender.phone))
+            print('text: ' + event.message.text)
+            # await event.reply(message)
+            await client.send_message(sender.username, message)
+        else:
+            print("==== New message (bot) ====")
+            print(str(event.message.date))
+            print("username: " + sender.username)
+            print('text: ' + event.message.text)
+
+
+client.run_until_disconnected()

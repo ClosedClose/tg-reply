48c483da40757e7e507d81781cf8723588fce56c
gribanovdan@gmail.com <V_eronika367>
2022-05-27 00:48:48 +0300

    Add event.is_channel verify

--- tg-reply.py
+++ tg-reply.py
@@ -15,21 +15,22 @@ if client.is_user_authorized():
 @client.on(events.NewMessage)
 async def normal_handler(event):
     sender = await client.get_entity(event.from_id)
-    if sender.username != myself.username:
-        if 'bot' not in sender.username:
-            print("==== New message ====")
-            print(str(event.message.date))
-            print("username: " + sender.username)
-            if sender.phone is not None:
-                print("phone: " + str(sender.phone))
-            print('text: ' + event.message.text)
-            # await event.reply(message)
-            await client.send_message(sender.username, message)
-        else:
-            print("==== New message (bot) ====")
-            print(str(event.message.date))
-            print("username: " + sender.username)
-            print('text: ' + event.message.text)
+    if not event.is_channel:
+        if sender.username != myself.username:
+            if 'bot' not in sender.username:
+                print("==== New message ====")
+                print(str(event.message.date))
+                print("username: " + sender.username)
+                if sender.phone is not None:
+                    print("phone: " + str(sender.phone))
+                print('text: ' + event.message.text)
+                # await event.reply(message)
+                await client.send_message(sender.username, message)
+            else:
+                print("==== New message (bot) ====")
+                print(str(event.message.date))
+                print("username: " + sender.username)
+                print('text: ' + event.message.text)
 
 
 client.run_until_disconnected()
